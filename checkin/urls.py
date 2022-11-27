74c9ed1923d68290f19af13f932990ff0af7cfe8
Jonathan Tumulak <tumulak.jonathan@gmail.com>
2022-11-27 15:21:57 +0800

    Add reports view and charts to visualize users check-ins

--- checkin/urls.py
+++ checkin/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="RegisterView"),
     path("checkin/", views.CheckinHomeView.as_view(), name="CheckinHomeView"),
     path("checkin/my/", views.MyCheckinView.as_view(), name="MyCheckinView"),
+    path("checkin/my/reports/", views.MyReportsView.as_view(), name="MyReportsView"),
     path("checkin/delete/<int:pk>/", views.DeleteCheckinView.as_view(), name="DeleteCheckinView"),
 ]
